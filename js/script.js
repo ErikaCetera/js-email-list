ec0908f940f060ff710f5b88fd0d35d03bf0113b
ErikaCetera <ceteraerika@gmail.com>
2024-11-18 16:39:15 +0100

    feat: bonus

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 const allLiElem = document.querySelectorAll("li");
+const btnElem = document.querySelector(".btn")
+const conteinerList = document.querySelector(".d-none")
 
-
-
+btnElem.addEventListener('click', function(event) {
+    event.preventDefault();
 for (let i = 0; i < allLiElem.length; i++) {
-    const curItem = allLiElem[i]
+    let curItem = allLiElem[i];
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
         const result = resp.data.response;
         curItem.innerHTML = result;
     })
+   conteinerList.classList.remove("d-none");
+
 
 };
+
+});
