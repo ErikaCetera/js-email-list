744db436eb78f5997bf69276fa521b706cceac91
ErikaCetera <ceteraerika@gmail.com>
2024-11-19 16:26:50 +0100

    feat: correzione

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
-const allLiElem = document.querySelectorAll("li");
-const btnElem = document.querySelector(".btn")
-const conteinerList = document.querySelector(".d-none")
-
-btnElem.addEventListener('click', function(event) {
-    event.preventDefault();
-for (let i = 0; i < allLiElem.length; i++) {
-    let curItem = allLiElem[i];
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
-        const result = resp.data.response;
-        curItem.innerHTML = result;
-    })
-   conteinerList.classList.remove("d-none");
+const btnElem = document.querySelector(".btn");
+const emailList = document.querySelector(".email-list");
+const loadingElem = document.getElementById("loading");
 
 
+let emails = [];
+const printEmails = () => {
+    emails.forEach((curmail) => {
+        emailList.innerHTML += `<li class="list-group-item">${curmail}</li>`;
+    });
 };
 
-});
+
+
+const getEmails = () => {
+    emails = [];
+    emailList.innerHTML = "";
+    loadingElem.style.display = "block";
+    for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(resp => {
+            const result = resp.data.response;
+            emails.push(result);
+            if (emails.length === 10) {
+                loadingElem.style.display = "none";
+                printEmails();
+            };
+        });
+
+
+
+    };
+
+}
+
+btnElem.addEventListener("click", getEmails);
